6e4489214df090ba09ef48dec9559c13930f4131
Fateme Arjomandi <farjomandy76@gmail.com>
2022-09-26 14:38:20 +0330

    test git diff

--- lib/main.dart
+++ lib/main.dart
@@ -108,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add_a_photo),
+        child: const Icon(Icons.add),
       ), // This Fateme trailing comma makes auto-formatting nicer for build methods.
     );
 
